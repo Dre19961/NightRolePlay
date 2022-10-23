35a4cd28c8b37f9f93f618b6819bf38ba901034a
MamaEV Andrew <andrey7135@ukr.net>
2022-10-23 15:50:47 +0300

    123

--- Lite/Utils/ChangePlayerDimension.cs
+++ Lite/Utils/ChangePlayerDimension.cs
@@ -45,15 +45,15 @@ namespace Lite.Utils
             {
                 string id = Timers.Start(2000, () =>
                 {
-                    if (player.Dimension == 0 && !player.IsSpawned()) { player.Kick(); return; }
-                    if (player.IsSpawned())
-                    {
-                        if (timerId.ContainsKey(player))
-                        {
-                            Timers.Stop(timerId[player]);
-                            timerId.Remove(player);
-                        }
-                    }
+                    if (player.Dimension == 0 && !player.IsSpawned()) player.Kick();
+                    //if (player.IsSpawned())
+                    //{
+                    //    if (timerId.ContainsKey(player))
+                    //    {
+                    //        Timers.Stop(timerId[player]);
+                    //        timerId.Remove(player);
+                    //    }
+                    //}
                 });
                 timerId.Add(player, id);
             }
