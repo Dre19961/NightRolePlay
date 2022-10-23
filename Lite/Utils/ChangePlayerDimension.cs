2ae64b53a4ffba1bd808d6c621c9e00e3c316a50
Jar Jar Biks <Flexonica93@gmail.com>
2022-10-23 16:02:39 +0300

    123

--- Lite/Utils/ChangePlayerDimension.cs
+++ Lite/Utils/ChangePlayerDimension.cs
@@ -34,14 +34,14 @@ namespace Lite.Utils
                 string id = Timers.Start(2000, () =>
                 {
                     if (player.Dimension == 0 && !player.IsSpawned()) player.Kick();
-                    //if (player.IsSpawned())
-                    //{
-                    //    if (timerId.ContainsKey(player))
-                    //    {
-                    //        Timers.Stop(timerId[player]);
-                    //        timerId.Remove(player);
-                    //    }
-                    //}
+                    if (player.IsSpawned())
+                    {
+                        if (timerId.ContainsKey(player))
+                        {
+                            Timers.Stop(timerId[player]);
+                            timerId.Remove(player);
+                        }
+                    }
                 });
                 timerId.Add(player, id);
             }
